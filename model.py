aef188c5285afc390506c8c4fadd0303dd6f7803
BoyanTTodorov <r0b0engi2t@gmail.com>
2024-10-21 10:32:19 +0200

    dump filter

--- model.py
+++ model.py
@@ -6,9 +6,15 @@ folder_path = r'Data'
 folder_data = os.listdir(folder_path)
 
 try:
-    df = pd.concat([pd.read_excel(folder_path+'/'+file, engine='openpyxl') for file in folder_data])
-    print(df.head())
+    df = pd.concat([pd.read_excel(folder_path+'/'+file, engine='openpyxl') for file in folder_data if file.endswith('.xlsm') or file.endswith('.xlsx')])
+    mask = df['Jobcode'].str.startswith('26')
+    filtered_df = df[mask].copy()
+    filtered_df.columns
+    filtered_df = filtered_df.rename(columns = {})
+    filtered_df.to_excel('guess.xlsx',index=False)
 except Exception as e:
     print(f'Exception occure: {e}')
 finally:
-    print(f'Data was readed')
+    print(f'Data was filtered')
+
+    
